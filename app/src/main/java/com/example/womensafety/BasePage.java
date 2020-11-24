aaeba292b833f75ca9f33380838c05234213fafe
Clarice <59640284+clarice99@users.noreply.github.com>
2020-11-24 21:01:29 +0530

    Updated with SMS and GPS location

--- app/src/main/java/com/example/womensafety/BasePage.java
+++ app/src/main/java/com/example/womensafety/BasePage.java
@@ -1,27 +1,51 @@
 package com.example.womensafety;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+//location imprts
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.content.Intent;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class BasePage extends AppCompatActivity {
 
+    private GpsTracker gpsTracker;
     private TextView userName;
     public static Boolean edit = false;
+    String longitudePublic;
+    String latitudePublic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_base_page);
+
+//        Asking for SMS permission
+        ActivityCompat.requestPermissions(BasePage.this,new String[]{Manifest.permission.SEND_SMS}, PackageManager.PERMISSION_GRANTED);
+
+//        Getting inApp permission for location and internet
+        try {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
 
         SharedPreferences getShared = getSharedPreferences("userData", MODE_PRIVATE);
         String name = getShared.getString("name", "DefaultName");
@@ -34,13 +58,6 @@ public class BasePage extends AppCompatActivity {
 
         setContentView(R.layout.activity_base_page);
 
-//        Button mButton = (Button) findViewById(R.id.);
-//        mButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(new Intent(BasePage.this, emergencyContacts.class));
-//            }
-//        });
 
         FloatingActionButton gohome = findViewById(R.id.gotohomepg);
         gohome.setOnClickListener(new View.OnClickListener() {
@@ -70,6 +87,46 @@ public class BasePage extends AppCompatActivity {
 
         });
 
+
+        FloatingActionButton Sos = findViewById(R.id.SOS);
+        Sos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                getLocation();
+                sendMsg(longitudePublic,latitudePublic);
+
+            }
+
+        });
+
+    }
+
+    public void getLocation(){
+        gpsTracker = new GpsTracker(BasePage.this);
+        if(gpsTracker.canGetLocation()){
+            double latitude = gpsTracker.getLatitude();
+            double longitude = gpsTracker.getLongitude();
+            latitudePublic =String.valueOf(latitude);
+            longitudePublic = String.valueOf(longitude);
+            Log.d("Location", "getLocation: long = "+longitudePublic+" lat = "+latitudePublic);
+        }else{
+            gpsTracker.showSettingsAlert();
+        }
+    }
+
+    public void sendMsg(String longitude,String latitude ) {
+        Toast.makeText(BasePage.this, "sms sent", Toast.LENGTH_SHORT).show();
+
+        String message = "Save Me!!! I'm at http://maps.google.com/?q=" + latitude + "," + longitude;
+        String number = "9869580141";
+        try{
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(number, null, message, null, null);
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
 
     }
 
